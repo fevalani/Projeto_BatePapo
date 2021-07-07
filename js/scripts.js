6a43454ee7cbcaea43409c9a43c08c38bd470ce1
Fernando Valani Cavalcante <valanife@gmail.com>
2021-07-07 11:11:21 -0300

    upload images for readme

--- js/scripts.js
+++ js/scripts.js
@@ -2,27 +2,32 @@ let usuario;
 let contatoSelecionado = "Todos";
 let visibilidadeSelecionada = "message";
 
+document
+  .querySelector(".texto-usuario")
+  .addEventListener("keypress", function (event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      login();
+    }
+  });
 
-document.querySelector(".texto-usuario").addEventListener("keypress", function(event){
-  if(event.keyCode === 13){
-    event.preventDefault();
-    login();
-  }
-});
-
-function login(){
+function login() {
   usuario = document.querySelector(".texto-usuario").value;
   const carregando = document.querySelector(".login div:last-child");
-  carregando.innerHTML = "<img src='./imagens/200.gif' alt='logo bate papo uol'>";
+  carregando.innerHTML =
+    "<img src='./imagens/200.gif' alt='logo bate papo uol'>";
 
-  const respostaLogin = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants', {name: usuario});
+  const respostaLogin = axios.post(
+    "https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants",
+    { name: usuario }
+  );
 
   respostaLogin.then(loginAceito);
   respostaLogin.catch(erroLogin);
 }
 
-function erroLogin(erro){
-  if(erro.response.status === 400){
+function erroLogin(erro) {
+  if (erro.response.status === 400) {
     alert("Ihh rapaz, esse nome já está sendo utilizado");
     const loginErrado = document.querySelector(".login div:last-child");
 
@@ -39,30 +44,40 @@ function loginAceito(respostaLogin) {
   setInterval(receberParticipantes, 10000);
 }
 
-function receberDados(){
-  const promessa = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages');
+function receberDados() {
+  const promessa = axios.get(
+    "https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages"
+  );
   promessa.then(distribuirChat);
 }
 
 function receberParticipantes() {
-  const participantes = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants');
+  const participantes = axios.get(
+    "https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants"
+  );
   participantes.then(distribuirContatos);
 }
 
-function estouOnline(){
-  const promessa = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status', {name: usuario});
+function estouOnline() {
+  const promessa = axios.post(
+    "https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status",
+    { name: usuario }
+  );
 }
 
-function distribuirChat(dados){
+function distribuirChat(dados) {
   const mensagens = dados.data;
   const esconder = document.querySelector(".login");
-  esconder.classList.add('escondido');
+  esconder.classList.add("escondido");
   const chat = document.querySelector(".chat");
 
-  chat.innerHTML = '';
+  chat.innerHTML = "";
 
   for (let i = 0; i < mensagens.length; i++) {
-    if(mensagens[i].type === 'private_message' && mensagens[i].to === usuario){
+    if (
+      mensagens[i].type === "private_message" &&
+      mensagens[i].to === usuario
+    ) {
       chat.innerHTML += `
         <li class="${mensagens[i].type}">
           <span class="hora">
@@ -76,7 +91,7 @@ function distribuirChat(dados){
           </span>
         </li>
       `;
-    }else if(mensagens[i].type === 'message'){
+    } else if (mensagens[i].type === "message") {
       chat.innerHTML += `
         <li class="${mensagens[i].type}">
           <span class="hora">
@@ -90,7 +105,7 @@ function distribuirChat(dados){
           </span>
         </li>
       `;
-    }else{
+    } else {
       chat.innerHTML += `
         <li class="${mensagens[i].type}">
           <span class="hora">
@@ -105,11 +120,11 @@ function distribuirChat(dados){
     }
   }
 
-  const ultima = document.querySelector(".chat li:last-child");
-  ultima.scrollIntoView();
+  const ultimaMensagem = document.querySelector(".chat li:last-child");
+  ultimaMensagem.scrollIntoView();
 }
 
-function distribuirContatos(participantes){
+function distribuirContatos(participantes) {
   const contatos = participantes.data;
   const perfis = document.querySelector(".contatos");
 
@@ -122,10 +137,10 @@ function distribuirContatos(participantes){
       <ion-icon class="verde escondido" name="checkmark-sharp"></ion-icon>
     </li>
   `;
-  
+
   for (let i = 0; i < contatos.length; i++) {
-    if(contatos[i].name !== usuario){
-      if(contatos[i].name === contatoSelecionado){
+    if (contatos[i].name !== usuario) {
+      if (contatos[i].name === contatoSelecionado) {
         perfis.innerHTML += `
         <li onclick="selecionarContato(this)">
             <div>
@@ -135,7 +150,7 @@ function distribuirContatos(participantes){
             <ion-icon class="verde escondido selecionado" name="checkmark-sharp"></ion-icon>
         </li>
       `;
-      }else{
+      } else {
         perfis.innerHTML += `
           <li onclick="selecionarContato(this)">
               <div>
@@ -151,23 +166,31 @@ function distribuirContatos(participantes){
 }
 
 document.querySelector(".texto-input").addEventListener("keydown", (e) => {
-  if(e.keyCode === 13){
+  if (e.keyCode === 13) {
     adicionarMensagem();
   }
 });
 
-function adicionarMensagem(){
+function adicionarMensagem() {
   const enviar = document.querySelector(".texto input").value;
-  const mensagem = {from: usuario, to: contatoSelecionado, text: enviar, type: visibilidadeSelecionada};
+  const mensagem = {
+    from: usuario,
+    to: contatoSelecionado,
+    text: enviar,
+    type: visibilidadeSelecionada,
+  };
 
-  const promessa = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages', mensagem);
+  const promessa = axios.post(
+    "https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages",
+    mensagem
+  );
   promessa.then(mensagemEnviada);
   promessa.catch(mensagemErro);
 
   document.querySelector(".texto input").value = "";
 }
 
-function mensagemErro(){
+function mensagemErro() {
   alert("Usuário offline");
   window.location.reload();
 }
@@ -176,58 +199,57 @@ function mensagemEnviada() {
   receberDados();
 }
 
-function abrirAba(){
-  const retirar = document.querySelector(".container-contatos");
-  retirar.classList.toggle('escondido');
-
+function abrirAba() {
+  const retirarAba = document.querySelector(".container-contatos");
+  retirarAba.classList.toggle("escondido");
 
   const trocarNome = document.querySelector(".aviso-mensagem");
-  if(visibilidadeSelecionada === "message"){
+  if (visibilidadeSelecionada === "message") {
     trocarNome.innerHTML = `Enviando para ${contatoSelecionado} (publicamente)`;
-  }else{
+  } else {
     trocarNome.innerHTML = `Enviando para ${contatoSelecionado} (reservadamente)`;
   }
 }
 
-function selecionarContato(valor){
+function selecionarContato(valor) {
   const mudar = valor.querySelector(".verde");
   const remover = document.querySelector(".selecionado");
-  
-  if(remover !== null){
-    if(remover.classList.contains('selecionado')){
-      remover.classList.remove('selecionado');
-      mudar.classList.add('selecionado');
-    }else{
-      mudar.classList.add('selecionado');
+
+  if (remover !== null) {
+    if (remover.classList.contains("selecionado")) {
+      remover.classList.remove("selecionado");
+      mudar.classList.add("selecionado");
+    } else {
+      mudar.classList.add("selecionado");
     }
-  }else{
-    mudar.classList.add('selecionado');
+  } else {
+    mudar.classList.add("selecionado");
   }
 
-  
-  contatoSelecionado = mudar.parentNode.querySelector(".nome-selecionado").innerHTML;
-
+  contatoSelecionado =
+    mudar.parentNode.querySelector(".nome-selecionado").innerHTML;
 }
 
-function selecionarVisibilidade(valor){
+function selecionarVisibilidade(valor) {
   const mudar = valor.querySelector(".verde");
   const remover = document.querySelector(".selecionar");
 
-  if(remover !== null){
-    if(remover.classList.contains('selecionar')){
-      remover.classList.remove('selecionar');
-      mudar.classList.add('selecionar');
-    }else{
-      mudar.classList.add('selecionar');
+  if (remover !== null) {
+    if (remover.classList.contains("selecionar")) {
+      remover.classList.remove("selecionar");
+      mudar.classList.add("selecionar");
+    } else {
+      mudar.classList.add("selecionar");
     }
-  }else{
-    mudar.classList.add('selecionar');
+  } else {
+    mudar.classList.add("selecionar");
   }
 
-  visibilidadeSelecionada = mudar.parentNode.querySelector(".nome-visibilidade").innerHTML;
-  if(visibilidadeSelecionada === "Público"){
+  visibilidadeSelecionada =
+    mudar.parentNode.querySelector(".nome-visibilidade").innerHTML;
+  if (visibilidadeSelecionada === "Público") {
     visibilidadeSelecionada = "message";
-  }else{
+  } else {
     visibilidadeSelecionada = "private-message";
   }
 }
